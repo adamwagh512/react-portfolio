0481a2d20469cfc2ff0e07f730ba2af76f700941
Adam Wagh <adamwagh@gmail.com>
2022-08-30 11:35:50 -0500

    added portfolio althought it still needs edits, only need to add contact

--- React Portfolio/aw-portfolio/src/components/pages/Portfolio.js
+++ React Portfolio/aw-portfolio/src/components/pages/Portfolio.js
@@ -1,22 +1,56 @@
 import React from 'react';
-
+import firstPortfolio from '../../images/firstPortfolio.jpg'
+import '../../styles/portfoliostyle.css'
 export default function Portfolio() {
   return (
-    <div>
+    <div className='portfolio'>
       <h1>Portfolio</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <div className='row'>
+      <div className="card">
+        <img className='pic' src={firstPortfolio} alt="First Portfolio"></img>
+        <h1>First Portfolio</h1>
+        <p class="description">This is the second website I ever built. I like to look at it and realize how far I have come in 3 months!</p>
+        <p>Check out the Github repo <a href="https://github.com/adamwagh512/second-challenge"target="_blank">here </a></p>
+        <p>Check out the deployed site <a href="https://adamwagh512.github.io/second-challenge/#work"target="_blank">here</a></p>
+      </div>
+      <div className="card">
+        <img className='pic' src={firstPortfolio} alt="First Portfolio"></img>
+        <h1>First Portfolio</h1>
+        <p class="description">This is the second website I ever built. I like to look at it and realize how far I have come in 3 months!</p>
+        <p>Check out the Github repo <a href="https://github.com/adamwagh512/second-challenge"target="_blank">here </a></p>
+        <p>Check out the deployed site <a href="https://adamwagh512.github.io/second-challenge/#work"target="_blank">here</a></p>
+      </div>
+      <div className="card">
+        <img className='pic' src={firstPortfolio} alt="First Portfolio"></img>
+        <h1>First Portfolio</h1>
+        <p class="description">This is the second website I ever built. I like to look at it and realize how far I have come in 3 months!</p>
+        <p>Check out the Github repo <a href="https://github.com/adamwagh512/second-challenge"target="_blank">here </a></p>
+        <p>Check out the deployed site <a href="https://adamwagh512.github.io/second-challenge/#work"target="_blank">here</a></p>
+      </div>
+      </div>
+      <div className='row'>
+      <div className="card">
+        <img className='pic' src={firstPortfolio} alt="First Portfolio"></img>
+        <h1>First Portfolio</h1>
+        <p class="description">This is the second website I ever built. I like to look at it and realize how far I have come in 3 months!</p>
+        <p>Check out the Github repo <a href="https://github.com/adamwagh512/second-challenge"target="_blank">here </a></p>
+        <p>Check out the deployed site <a href="https://adamwagh512.github.io/second-challenge/#work"target="_blank">here</a></p>
+      </div>
+      <div className="card">
+        <img className='pic' src={firstPortfolio} alt="First Portfolio"></img>
+        <h1>First Portfolio</h1>
+        <p class="description">This is the second website I ever built. I like to look at it and realize how far I have come in 3 months!</p>
+        <p>Check out the Github repo <a href="https://github.com/adamwagh512/second-challenge"target="_blank">here </a></p>
+        <p>Check out the deployed site <a href="https://adamwagh512.github.io/second-challenge/#work"target="_blank">here</a></p>
+      </div>
+      <div className="card">
+        <img className='pic' src={firstPortfolio} alt="First Portfolio"></img>
+        <h1>First Portfolio</h1>
+        <p class="description">This is the second website I ever built. I like to look at it and realize how far I have come in 3 months!</p>
+        <p>Check out the Github repo <a href="https://github.com/adamwagh512/second-challenge"target="_blank">here </a></p>
+        <p>Check out the deployed site <a href="https://adamwagh512.github.io/second-challenge/#work"target="_blank">here</a></p>
+        </div>
+      </div>
     </div>
   );
 }
